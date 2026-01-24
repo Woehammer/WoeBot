292504666d9725ec32d45579b52f60711f9534a4
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-01-24 23:49:37 +0000

    Update formations.js

--- src/data/aos/formations.js
+++ src/data/aos/formations.js
@@ -3,6 +3,66 @@
 // PURPOSE: Canonical formation names + aliases
 // ==================================================
 
+export const FORMATIONS_AOS = [
+  {
+    name: "Bloodbound Warhorde",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Bloodbound Warhorde",
+    ],
+  },
+  {
+    name: "Brass Stampede",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Brass Stampede",
+    ],
+  },
+  {
+    name: "Khornate Legion",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Khornate Legion",
+    ],
+  },
+  {
+    name: "Murderhost",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Murderhost",
+      "Murder Host",
+    ],
+  },
+  {
+    name: "The Goretide",
+    faction: "Blades of Khorne",
+    aliases: [
+      "The Goretide",
+    ],
+  },
+  {
+    name: "Tournament of Skulls",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Tournament of Skulls",
+    ],
+  },
+  {
+    name: "Gorechosen Champions",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Gorechosen Champions",
+    ],
+  },
+  {
+    name: "The Baleful Lords",
+    faction: "Blades of Khorne",
+    aliases: [
+      "The Baleful Lords",
+    ],
+  },
+];
+
 // ==================================================
 // FORMATION DEFINITIONS
 // ==================================================
@@ -10,3 +70,5 @@
 // ==================================================
 // EXPORTS
 // ==================================================
+
+export default FORMATIONS_AOS;
