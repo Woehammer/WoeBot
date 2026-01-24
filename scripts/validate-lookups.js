b0da68d57f084103aef5b5fbc53329b9f837832e
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-01-24 23:22:06 +0000

    Create validate-lookups.js

---
+++ scripts/validate-lookups.js
@@ -0,0 +1,4 @@
+// ==================================================
+// SCRIPT: VALIDATE LOOKUPS
+// PURPOSE: Detect alias collisions and invalid entries
+// ==================================================
