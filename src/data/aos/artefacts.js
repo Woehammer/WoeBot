83be8e98aee37020fcea12c4587e37d07214ae5d
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-01-25 00:00:20 +0000

    Create artefacts.js

---
+++ src/data/aos/artefacts.js
@@ -0,0 +1,73 @@
+// ==================================================
+// LOOKUP: AOS ARTEFACTS OF POWER
+// PURPOSE: Canonical artefacts of power names + aliases
+// ==================================================
+
+export const ARTEFACTS_AOS = [
+  {
+    name: "Ar'Gath, The King of Blades",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Ar'Gath, The King of Blades",
+    ],
+  },
+  {
+    name: "Collar of Contempt",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Collar of Contempt",
+    ],
+  },
+  {
+    name: "Butcher's Blade",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Butcher's Blade",
+    ],
+  },
+  {
+    name: "Warmonger's Icon",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Warmonger's Icon",
+    ],
+  },
+  {
+    name: "The Goretide",
+    faction: "Blades of Khorne",
+    aliases: [
+      "The Goretide",
+    ],
+  },
+  {
+    name: "The Scarring Blade",
+    faction: "Blades of Khorne",
+    aliases: [
+      "The Scarring Blade",
+    ],
+  },
+  {
+    name: "Bloodmist Skull",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Bloodmist Skull",
+    ],
+  },
+  {
+    name: "Crown of the Slaughterborn",
+    faction: "Blades of Khorne",
+    aliases: [
+      "Crown of the Slaughterborn",
+    ],
+  },
+];
+
+// ==================================================
+// ARTEFACTS DEFINITIONS
+// ==================================================
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default ARTEFACTS_AOS;
