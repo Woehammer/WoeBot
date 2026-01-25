0464399e35e57c5e411a26c39fab95a6480de726
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-01-25 00:21:38 +0000

    Create terrain.js

---
+++ src/data/aos/terrain.js
@@ -0,0 +1,24 @@
+// ==================================================
+// LOOKUP: AOS FACTION TERRAIN
+// PURPOSE: Canonical faction terrain names + alias definitions
+// ==================================================
+
+// ==================================================
+// FACTION TERAIN DEFINITIONS
+// ==================================================
+
+export const TERRAIN_AOS = [
+    {
+    name: "Skull Altar",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skull altar",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default TERRAIN_AOS;
