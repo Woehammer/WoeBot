cd3d681bff3ebb7ca32830df056036000794be62
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-01-30 17:12:26 +0000

    Update daughters_of_khaine.js

--- src/data/aos/warscrolls/daughters_of_khaine.js
+++ src/data/aos/warscrolls/daughters_of_khaine.js
@@ -22,6 +22,48 @@ const DAUGHTERS_OF_KHAINE = [
       "bloodwrack shrine",
     ],
   },
+{
+    name: "Hag Queen",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "hag queen",
+    ],
+  },
+{
+    name: "Hag Queen on Cauldron of Blood",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "hag queen on cauldron of blood",
+    ],
+  },
+{
+    name: "High Gladiatrix",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "high gladiatrix",
+    ],
+  },
+{
+    name: "Krethusa the Croneseer",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "krethusa the croneseer",
+    ],
+  },
+{
+    name: "Melusai Ironscale",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "melusai ironscale",
+    ],
+  },
+{
+    name: "Morathi-Khaine",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "morathi-khaine",
+    ],
+  },
 ];
 
 // ==================================================
