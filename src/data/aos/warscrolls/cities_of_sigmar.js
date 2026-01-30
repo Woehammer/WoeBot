cfeb2291ad434c04072ca489c038fa77e1c7ae9c
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-01-30 16:59:16 +0000

    Create cities_of_sigmar.js

---
+++ src/data/aos/warscrolls/cities_of_sigmar.js
@@ -0,0 +1,241 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — CITIES OF SIGMAR
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const CITIES_OF_SIGMAR = [
+    {
+    name: "Lord of Khorne on Juggernaut",
+    faction: "Blades of Khorne",
+    aliases: [
+      "lord of khorne on juggernaut",
+    ],
+  },
+  {
+    name: "Skarr Bloodwrath",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skarr bloodwrath",
+    ],
+  },
+  {
+    name: "Skullmaster, Herald of Khorne",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skullmaster, herald of khorne",
+    ],
+  },
+  {
+    name: "Bloodmaster, Herald of Khorne",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodmaster, herald of khorne",
+    ],
+  },
+  {
+    name: "Bloodsecrator",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodsecrator",
+    ],
+  },
+  {
+    name: "Bloodstoker",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodstoker",
+    ],
+  },
+  {
+    name: "Bloodthirster of Insensate Rage",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodthirster of insensate rage",
+    ],
+  },
+  {
+    name: "Bloodthirster of Unfettered Fury",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodthirster of unfettered fury",
+    ],
+  },
+  {
+    name: "Deathbringer",
+    faction: "Blades of Khorne",
+    aliases: [
+      "deathbringer",
+    ],
+  },
+  {
+    name: "Herald of Khorne on Blood Throne",
+    faction: "Blades of Khorne",
+    aliases: [
+      "herald of khorne on blood throne",
+    ],
+  },
+   {
+    name: "Karanak",
+    faction: "Blades of Khorne",
+    aliases: [
+      "karanak",
+    ],
+  },
+   {
+    name: "Mighty Lord of Khorne",
+    faction: "Blades of Khorne",
+    aliases: [
+      "mighty lord of khorne",
+    ],
+  },
+   {
+    name: "Realmgore Ritualist",
+    faction: "Blades of Khorne",
+    aliases: [
+      "realmgore ritualist",
+    ],
+  },
+    {
+    name: "Skarbrand",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skarbrand",
+    ],
+  },
+    {
+    name: "Skullgrinder",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skullgrinder",
+    ],
+  },
+    {
+    name: "Skulltaker",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skulltaker",
+    ],
+  },
+    {
+    name: "Slaughterpriest",
+    faction: "Blades of Khorne",
+    aliases: [
+      "slaughterpriest",
+    ],
+  },
+    {
+    name: "Wrath of Khorne Bloodthirster",
+    faction: "Blades of Khorne",
+    aliases: [
+      "wrath of khorne bloodthirster",
+    ],
+  },
+    {
+    name: "Blood Warriors",
+    faction: "Blades of Khorne",
+    aliases: [
+      "blood warriors",
+    ],
+  },
+    {
+    name: "Bloodletters",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodletters",
+    ],
+  },
+    {
+    name: "Bloodreavers",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodreavers",
+    ],
+  },
+    {
+    name: "Claws of Karanak",
+    faction: "Blades of Khorne",
+    aliases: [
+      "claws of karanak",
+    ],
+  },
+    {
+    name: "Skullreapers",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skullreapers",
+    ],
+  },
+    {
+    name: "Wrathmongers",
+    faction: "Blades of Khorne",
+    aliases: [
+      "wrathmongers",
+    ],
+  },
+    {
+    name: "Bloodcrushers",
+    faction: "Blades of Khorne",
+    aliases: [
+      "bloodcrushers",
+    ],
+  },
+    {
+    name: "Mighty Skullcrushers",
+    faction: "Blades of Khorne",
+    aliases: [
+      "mighty skullcrushers",
+    ],
+  },
+    {
+    name: "Flesh Hounds",
+    faction: "Blades of Khorne",
+    aliases: [
+      "flesh hounds",
+    ],
+  },
+    {
+    name: "Khorgorath",
+    faction: "Blades of Khorne",
+    aliases: [
+      "khorgorath",
+    ],
+  },
+    {
+    name: "Skull Cannon",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skull cannon",
+    ],
+  },
+    {
+    name: "Skull Altar",
+    faction: "Blades of Khorne",
+    aliases: [
+      "skull altar",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Mighty Lord of Khorne",
+    faction: "Blades of Khorne",
+    aliases: [
+      "scourge of ghyran mighty lord of khorne",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Bloodcrushers",
+    faction: "Blades of Khorne",
+    aliases: [
+      "scourge of ghyran bloodcrushers",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default CITIES_OF_SIGMAR;
