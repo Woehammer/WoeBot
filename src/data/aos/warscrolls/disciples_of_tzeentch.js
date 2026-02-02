61aec168a1673ac62deb6a1d17d9b2ff733d9d78
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-02 00:03:55 +0000

    Create disciples_of_tzeentch.js

---
+++ src/data/aos/warscrolls/disciples_of_tzeentch.js
@@ -0,0 +1,52 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — DISCIPLES OF TZEENTCH
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const DISCIPLES_OF_TZEENTCH = [
+          {
+    name: "Changecaster, Herald of Tzeentch",
+    faction: "Disciples of Tzeentch",
+    aliases: [
+      "changecaster, herald of tzeentch",
+    ],
+  },
+  {
+    name: "Curseling, Eye of Tzeentch",
+    faction: "Disciples of Tzeentch",
+    aliases: [
+      "curseling, eye of tzeentch",
+    ],
+  },
+{
+    name: "Fateskimmer, Herald of Tzeentch on Burning Chariot",
+    faction: "Disciples of Tzeentch",
+    aliases: [
+      "fateskimmer, herald of tzeentch on burning chariot",
+    ],
+  },
+{
+    name: "Gaunt Summoner",
+    faction: "Disciples of Tzeentch",
+    aliases: [
+      "gaunt summoner",
+    ],
+  },
+{
+    name: "Gaunt Summoner on Disc of Tzeentch",
+    faction: "Disciples of Tzeentch",
+    aliases: [
+      "gaunt summoner on disc of tzeentch",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default DISCIPLES_OF_TZEENTCH;
