cb36d01e09bbe1068c03c311955c82b51b5aa05d
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-02 00:05:48 +0000

    Update warscrolls.js

--- src/data/aos/warscrolls.js
+++ src/data/aos/warscrolls.js
@@ -6,6 +6,7 @@
 import bladesOfKhorne from "./warscrolls/blades_of_khorne.js";
 import citiesOfSigmar from "./warscrolls/cities_of_sigmar.js";
 import daughtersOfKhaine from "./warscrolls/daughters_of_khaine.js";
+import disciplesOfTzeentch from "./warscrolls/disciples_of_tzeentch.js";
 
 // ==================================================
 // WARSCROLL DEFINITIONS (MERGED)
@@ -15,6 +16,7 @@ export const WARSCROLLS_AOS = [
   ...bladesOfKhorne,
   ...citiesOfSigmar,
   ...daughtersOfKhaine,
+  ...disciplesOfTzeentch,
 ];
 
 // ==================================================
