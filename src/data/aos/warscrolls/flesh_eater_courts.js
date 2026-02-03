41eb4115cac25b7fa8393467d2314f5c6c1d1657
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-03 23:03:52 +0000

    Create flesh_eater_courts.js

---
+++ src/data/aos/warscrolls/flesh_eater_courts.js
@@ -0,0 +1,24 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — FLESH EATER COURTS
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const FLESH_EATER_COURTS = [
+    {
+    name: "Abhorrant Archregent",
+    faction: "Flesh-eater Courts",
+    aliases: [
+      "abhorrant archregent",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default FLESH_EATER_COURTS;
