afac6a356582615e2f9c57375677f290ceef3104
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-04 15:02:33 +0000

    Create gloomspite_gitz.js

---
+++ src/data/aos/warscrolls/gloomspite_gitz.js
@@ -0,0 +1,290 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — GLOOMSPITE GITZ
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const GLOOMSPITE_GITZ = [
+    {
+    name: "Dankhold Troggboss",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "dankhold troggboss",
+    ],
+  },
+{
+    name: "Droggz Da Sunchompa",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "droggz da sunchompa",
+    ],
+  },
+{
+    name: "Frazzlegit Shaman on War-Wheela",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "frazzlegit shaman on war-wheela",
+    ],
+  },
+{
+    name: "Fungoid Cave-Shaman",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "fungoid cave-shaman",
+    ],
+  },
+{
+    name: "Kragnos, the End of Empires",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "Kragnos, the end of empires",
+    ],
+  },
+{
+    name: "Loonboss",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "loonboss",
+    ],
+  },
+{
+    name: "Loonboss on Giant Cave Squig",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "loonboss on giant cave squig",
+    ],
+  },
+{
+    name: "Loonboss on Mangler Squigs",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "loonboss on mangler squigs",
+    ],
+  },
+{
+    name: "Rabble-Rowza",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "rabble-rowza",
+    ],
+  },
+{
+    name: "Skragrott, the Loonking",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "skragrott, the loonking",
+    ],
+  },
+{
+    name: "Snarlboss",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "snarlboss",
+    ],
+  },
+{
+    name: "Snarlboss on War-Wheela",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "snarlboss on war-wheela",
+    ],
+  },
+{
+    name: "Squigboss with Gnasha-Squig",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "squigboss with gnasha-squig",
+    ],
+  },
+{
+    name: "Trugg, the Troggoth King",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "trugg, the troggoth king",
+    ],
+  },
+{
+    name: "Webspinner Shaman",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "webspinner shaman",
+    ],
+  },
+{
+    name: "Webspinner Shaman on Arachnarok Spider",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "webspinner shaman on arachnarok spider",
+    ],
+  },
+{
+    name: "Fellwater Troggoths",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "fellwater troggoths",
+    ],
+  },
+{
+    name: "Gobbapalooza",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "gobbapalooza",
+    ],
+  },
+{
+    name: "Loonsmasha Fanatics",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "loonsmasha fanatics",
+    ],
+  },
+{
+    name: "Moonclan Shootas",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "moonclan shootas",
+    ],
+  },
+{
+    name: "Moonclan Stabbas",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "moonclan stabbas",
+    ],
+  },
+{
+    name: "Rockgut Troggoths",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "rockgut troggoths",
+    ],
+  },
+{
+    name: "Sneaky Snufflers",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "sneaky snufflers",
+    ],
+  },
+{
+    name: "Sporesplatta Fanatics",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "sporesplatta fanatics",
+    ],
+  },
+{
+    name: "Boingrot Bounderz",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "boingrot bounderz",
+    ],
+  },
+{
+    name: "Snarlfang Riders",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "snarlfang riders",
+    ],
+  },
+{
+    name: "Snarlfang Cavalry",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "snarlfang cavalry",
+    ],
+  },
+{
+    name: "Spider Riders",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "spider riders",
+    ],
+  },
+{
+    name: "Squig Hoppers",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "squig hoppers",
+    ],
+  },
+{
+    name: "Wolfgit Retinue",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "wolfgit retinue",
+    ],
+  },
+{
+    name: "Squig Herd",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "squig herd",
+    ],
+  },
+{
+    name: "Arachnarok Spider with Flinger",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "arachnarok spider with flinger",
+    ],
+  },
+{
+    name: "Arachnarok Spider with Spiderfang Warparty",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "arachnarok spider with spiderfang warparty",
+    ],
+  },
+{
+    name: "Dankhold Troggoth",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "dankhold troggoth",
+    ],
+  },
+{
+    name: "Mangler Squigs",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "Mangler Squigs",
+    ],
+  },
+{
+    name: "Skitterstrand Arachnarok",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "skitterstrand arachnarok",
+    ],
+  },
+{
+    name: "Doom Diver Catapult",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "doom diver catapult",
+    ],
+  },
+{
+    name: "Sunsteala Wheelas",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "sunsteala wheelas",
+    ],
+  },
+{
+    name: "Bad Moon Loonshrine",
+    faction: "Gloomspite Gitz",
+    aliases: [
+      "bad moon loonshrine",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default GLOOMSPITE_GITZ;
