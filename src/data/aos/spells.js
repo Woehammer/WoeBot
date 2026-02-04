a783a5439a8c6fab33d90537151bedba6d062de4
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-04 12:17:01 +0000

    Create spells.js

---
+++ src/data/aos/spells.js
@@ -0,0 +1,53 @@
+// ==================================================
+// LOOKUP: AOS SPELLS
+// PURPOSE: Canonical spell lore names + aliases
+// ==================================================
+
+// ==================================================
+// SPELL LORE DEFINITIONS
+// ==================================================
+
+export const SPELLS_AOS = [
+  {
+    name: "Manifestations of Khaine",
+    aliases: [
+      "manifestatations of khaine",
+    ],
+  },
+  {
+    name: "Judgements of Khorne",
+    aliases: [
+      "judgements of khorne",
+    ],
+  },
+  {
+    name: "Manifestations of Tzeentch",
+    aliases: [
+      "manifestations of tzeentch",
+    ],
+  },
+{
+    name: "Manifested Insanity",
+    aliases: [
+      "manifested insanity",
+    ],
+  },
+{
+    name: "Magmic Invocations",
+    aliases: [
+      "magmic invocations",
+    ],
+  },
+{
+    name: "Dank Manifestations",
+    aliases: [
+      "dank manifestations",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default SPELLS_AOS;
