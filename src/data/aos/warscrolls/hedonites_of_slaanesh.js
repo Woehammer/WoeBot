3edd713227d13f37ba9014e701d9120783aa656a
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-04 16:20:49 +0000

    Create hedonites_of_slaanesh.js

---
+++ src/data/aos/warscrolls/hedonites_of_slaanesh.js
@@ -0,0 +1,186 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — HEDONITES OF SLAANESH
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const HEDONITES_OF_SLAANESH = [
+          {
+    name: "Bladebringer, Herald on Exalted Chariot",
+    faction: "Hedonites of Slaanesh",
+    aliases: [
+      "bladebringer, herald on exalted chariot",
+    ],
+  },
+  {
+    name: "Bloodwrack Shrine",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "bloodwrack shrine",
+    ],
+  },
+{
+    name: "Hag Queen",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "hag queen",
+    ],
+  },
+{
+    name: "Hag Queen on Cauldron of Blood",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "hag queen on cauldron of blood",
+    ],
+  },
+{
+    name: "High Gladiatrix",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "high gladiatrix",
+    ],
+  },
+{
+    name: "Krethusa the Croneseer",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "krethusa the croneseer",
+    ],
+  },
+{
+    name: "Melusai Ironscale",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "melusai ironscale",
+    ],
+  },
+{
+    name: "Morathi-Khaine",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "morathi-khaine",
+    ],
+  },
+{
+    name: "Slaughter Queen",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "slaughter queen",
+    ],
+  },
+{
+    name: "Slaughter Queen on Cauldron of Blood",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "slaughter queen on cauldron of blood",
+    ],
+  },
+{
+    name: "The Shadow Queen",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "the shadow queen",
+    ],
+  },
+{
+    name: "Blood Sisters",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "blood sisters",
+    ],
+  },
+{
+    name: "Blood Stalkers",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "blood stalkers",
+    ],
+  },
+{
+    name: "Khainite Shadowstalkers",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "khainite shadowstalkers",
+    ],
+  },
+{
+    name: "Khinerai Heartrenders",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "khinerai heartrenders",
+    ],
+  },
+{
+    name: "Khinerai Lifetakers",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "khinerai lifetakers",
+    ],
+  },
+{
+    name: "Sisters of Slaughter with Bladed Bucklers",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "sisters of slaughter with bladed bucklers",
+    ],
+  },
+{
+    name: "Sisters of Slaughter with Sacrificial Knives",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "sisters of slaughter with aacrificial knives",
+    ],
+  },
+{
+    name: "Witch Aelves with Bladed Bucklers",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "witch aelves with bladed bucklers",
+    ],
+  },
+{
+    name: "Witch Aelves with Paired Sciansá",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "witch aelves with paired sciansá",
+      "witch aelves with paired sciansa",
+    ],
+  },
+{
+    name: "Doomfire Warlocks",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "doomfire warlocks",
+    ],
+  },
+{
+    name: "Avatar of Khaine",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "avatar of khaine",
+    ],
+  },
+{
+    name: "Scourge of Ghyran Bloodwrack Shrine",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "scourge of ghyran bloodwrack shrine",
+    ],
+  },
+{
+    name: "Scourge of Ghyran Krethusa the Croneseer",
+    faction: "Daughters of Khaine",
+    aliases: [
+      "scourge of ghyran krethusa the croneseer",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default HEDONITES_OF_SLAANESH;
