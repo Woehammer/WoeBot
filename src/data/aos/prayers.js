9241a541d7146a178d616bfe144967dc44b915dc
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-04 12:15:08 +0000

    Create prayers.js

---
+++ src/data/aos/prayers.js
@@ -0,0 +1,95 @@
+// ==================================================
+// LOOKUP: AOS PRAYERS
+// PURPOSE: Canonical prayer lore names + aliases
+// ==================================================
+
+// ==================================================
+// PRAYER LORE DEFINITIONS
+// ==================================================
+
+export const PRAYERS_AOS = [
+  {
+    name: "Blood Blessings of Khorne",
+    aliases: [
+      "blood blessings of khorne",
+    ],
+  },
+  {
+    name: "Gifts of the Blood God",
+    aliases: [
+      "gifts of the blood god",
+    ],
+  },
+  {
+    name: "Gorechosen Champions Prayer Lore",
+    aliases: [
+      "gorechosen champions prayer lore",
+    ],
+  },
+{
+    name: "Scriptures of Sigmar",
+    aliases: [
+      "scriptures of sigmar",
+    ],
+  },
+{
+    name: "Prayers of the Khainite Cult",
+    aliases: [
+      "prayers of the khainite cult",
+    ],
+  },
+{
+    name: "Bloodshadow Rites",
+    aliases: [
+      "bloodshadow rites",
+    ],
+  },
+{
+    name: "The Croneseer's Pariahs Prayer Lore",
+    aliases: [
+      "the croneseer's pariahs prayer lore",
+    ],
+  },
+{
+    name: "Rites of Delusion",
+    aliases: [
+      "rites of delusion",
+    ],
+  },
+{
+    name: "The Equinox Feast Prayer Lore",
+    aliases: [
+      "the equinox feast prayer lore",
+    ],
+  },
+{
+    name: "The Knights of New Summercourt Prayer Lore",
+    aliases: [
+      "the knights of new summercourt prayer lore",
+    ],
+  },
+{
+    name: "Zharrgrim Blessings",
+    aliases: [
+      "zharrgrim blessings",
+    ],
+  },
+{
+    name: "Vulkyn Gifts",
+    aliases: [
+      "vulkyn gifts",
+    ],
+  },
+{
+    name: " Lofnir Drothkeepers Prayer Lore",
+    aliases: [
+      "lofnir drothkeepers prayer lore",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default PRAYERS_AOS;
