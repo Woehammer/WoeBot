c94d772469578afccb9e308c7706bc9acced0a10
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-09 15:43:11 +0000

    Update kharadron_overlords.js

--- src/data/aos/warscrolls/kharadron_overlords.js
+++ src/data/aos/warscrolls/kharadron_overlords.js
@@ -18,126 +18,147 @@ const KHARADRON_OVERLORDS = [
   {
     name: "Aetheric Navgator",
     faction: "Kharadron Overlords",
+    aliases: [
       "aetherix navigator",
     ],
   },
   {
     name: "Arkanaut Admiral",
     faction: "Kharadron Overlords",
+    aliases: [
       "arkanaut admiral",
     ],
   },
    {
     name: "Brokk Grungsson, Lord-Magnate of Barak-Nar",
     faction: "Kharadron Overlords",
+    aliases: [
       "brokk grungsson, lord-magnate of barak-nar",
     ],
   },
    {
     name: "Codewright",
     faction: "Kharadron Overlords",
+    aliases: [
       "codewright",
     ],
   },
   {
     name: "Drekki Flynt",
     faction: "Kharadron Overlords",
+    aliases: [
       "drekki flynt",
     ],
   },
   {
     name: "Endrinmaster with Dirigible Suit",
     faction: "Kharadron Overlords",
+    aliases: [
       "endrinmaster with dirigible suit",
     ],
   },
   {
     name: "Endrinmaster with Endrinharness",
     faction: "Kharadron Overlords",
+    aliases: [
       "endrinmaster with endrinharness",
     ],
   },
   {
     name: "Null-Khemist",
     faction: "Kharadron Overlords",
+    aliases: [
       "null-khemist",
     ],
   },
   {
     name: "Arkanaut Company",
     faction: "Kharadron Overlords",
+    aliases: [
       "arkanaut company",
     ],
   },
   {
     name: "Endrinriggers",
     faction: "Kharadron Overlords",
+    aliases: [
       "endrinriggers",
     ],
   },
   {
     name: "Grundstok Thunderers",
     faction: "Kharadron Overlords",
+    aliases: [
       "grundstok thunderers",
     ],
   },
   {
     name: "Skywardens",
     faction: "Kharadron Overlords",
+    aliases: [
       "skywardens",
     ],
   },
   {
     name: "Vongrim Harpoon Crew",
     faction: "Kharadron Overlords",
-      "congrim harpoon crew",
+    aliases: [
+      "vongrim harpoon crew",
     ],
   },
   {
     name: "Vongrim Salvagers",
     faction: "Kharadron Overlords",
+    aliases: [
       "vongrim salvagers",
     ],
   },
   {
     name: "Arkanaut Frigate",
     faction: "Kharadron Overlords",
+    aliases: [
       "arkanaut frigate",
     ],
   },
   {
     name: "Arkanaut Ironclad",
     faction: "Kharadron Overlords",
+    aliases: [
       "arkanaut ironclad",
     ],
   },
   {
     name: "Auto-Endrin",
     faction: "Kharadron Overlords",
+    aliases: [
       "auto-endrin",
     ],
   },
   {
     name: "Grundstok Gunhauler",
     faction: "Kharadron Overlords",
+    aliases: [
       "grundstok gunhauler",
     ],
   },
   {
     name: "Scourge of Ghyran Codewright",
     faction: "Kharadron Overlords",
+    aliases: [
       "scourge of ghyran codewright",
     ],
   },
   {
     name: "Zontari Endrin Dock",
     faction: "Kharadron Overlords",
+    aliases: [
       "zontari endrin dock",
     ],
   },
   {
     name: "Scourge of Ghyran Grundstok Gunhauler",
     faction: "Kharadron Overlords",
+    aliases: [
       "scourge of ghyran grundstok gunhauler",
     ],
   },
