0a215ee21f9bc27aff92798d1db0d0c8e16125e4
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-09 16:01:51 +0000

    Create lumineth_realmlords.js

---
+++ src/data/aos/warscrolls/lumineth_realmlords.js
@@ -0,0 +1,206 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — LUMINETH REALM-LORDS
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const LUMINETH_REALMLORDS = [
+    {
+    name: "Alarith Stonemage",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "alarith stonemage",
+    ],
+  },
+  {
+    name: "Archmage Teclis and Celennar, Spirit of Hysh",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "archmage teclis and celennar, spirit of hysh",
+    ],
+  },
+  {
+    name: "Avalenor, The Stoneheart King",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "avalenor, the stoneheart king",
+    ],
+  },
+   {
+    name: "Ellania and Ellathor, Eclipsian Warsages",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "ellania and ellathor, eclipsian warsages",
+    ],
+  },
+   {
+    name: "Hurakan Windmage",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "hurakan windmage",
+    ],
+  },
+  {
+    name: "Lyrior Uthralle, Warden of Ymetrica",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "lyrior uthralle, warden of ymetrica",
+    ],
+  },
+  {
+    name: "Sincari Calligrave",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "sincari calligrave",
+    ],
+  },
+  {
+    name: "Scinari Cathallar",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "scinari cathallar",
+    ],
+  },
+  {
+    name: "Scinari Enlightener",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "scinari enlightener",
+    ],
+  },
+  {
+    name: "Scinari Loreseeker",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "scinari loreseeker",
+    ],
+  },
+  {
+    name: "Severith, Lord of the Seventh Wind",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "severith, lord of the seventh wind",
+    ],
+  },
+  {
+    name: "The Light of Eltharion",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "the light of eltharion",
+    ],
+  },
+  {
+    name: "Vanari Bannerblade",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari bannerblade",
+    ],
+  },
+  {
+    name: "Vanari Lord Regent",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari lord regent",
+    ],
+  },
+  {
+    name: "Alarith Stoneguard",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "alarith stoneguard",
+    ],
+  },
+  {
+    name: "Vanari Auralan Sentinels",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari auralan sentinels",
+    ],
+  },
+  {
+    name: "Vanari Auralan Wardens",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari auralan wardens",
+    ],
+  },
+  {
+    name: "Vanari Bladelords",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari bladelords",
+    ],
+  },
+  {
+    name: "Ydrilan Riverblades",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "ydrilan riverblades",
+    ],
+  },
+  {
+    name: "Hurakan Windchargers",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "hurakan windchargers",
+    ],
+  },
+  {
+    name: "Vanari Dawnriders",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari dawnriders",
+    ],
+  },
+  {
+    name: "Alarith Spirit of the Mountain",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "alarith spirit of the mountain",
+    ],
+  },
+  {
+    name: "Hurakan Spirit of the Wind",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "hurakan spirit of the wind",
+    ],
+  },
+  {
+    name: "Vanari Starshard Ballista",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "vanari starshard ballista",
+    ],
+  },
+  {
+    name: "Shrine Luminor",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "shrine luminor",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran The Light of Eltharion",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "scourge of ghyran the light of eltharion",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Vanari Bannerblade",
+    faction: "Lumineth Realm-lords",
+	aliases: [
+      "scourge of ghyran vanari bannerblade",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default LUMINETH_REALMLORDS;
