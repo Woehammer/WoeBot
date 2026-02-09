c46297d90ce83ab39566bb61bdd55c3e8ca13405
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-09 23:05:39 +0000

    Create nighthaunt.js

---
+++ src/data/aos/warscrolls/nighthaunt.js
@@ -0,0 +1,227 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — NIGHTHAUNT
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const NIGHTHAUNT = [
+    {
+    name: "Cairn Wraith",
+    faction: "Nighthaunt",
+	aliases: [
+      "cairn wraith",
+    ],
+  },
+  {
+    name: "Tomb Banshee",
+    faction: "Nighthaunt",
+	aliases: [
+      "tomb banshee",
+    ],
+  },
+  {
+    name: "Glaivewraith Stalkers",
+    faction: "Nighthaunt",
+	aliases: [
+      "glaivewraith stalkers",
+    ],
+  },
+   {
+    name: "Awlrach the Drowner",
+    faction: "Nighthaunt",
+	aliases: [
+      "awlrach the drowner",
+    ],
+  },
+   {
+    name: "Guardian of Souls",
+    faction: "Nighthaunt",
+	aliases: [
+      "guardian of souls",
+    ],
+  },
+  {
+    name: "Knight of Shrouds",
+    faction: "Nighthaunt",
+	aliases: [
+      "knight of shrouds",
+    ],
+  },
+  {
+    name: "Knight of Shrouds on Ethereal Steed",
+    faction: "Nighthaunt",
+	aliases: [
+      "knight of shrouds on ethereal steed",
+    ],
+  },
+  {
+    name: "Krulghast Cruciator",
+    faction: "Nighthaunt",
+	aliases: [
+      "krulghast cruciator",
+    ],
+  },
+  {
+    name: "Kurdoss Valentian, the Craven King",
+    faction: "Nighthaunt",
+	aliases: [
+      "kurdoss valentian, the craven king",
+    ],
+  },
+  {
+    name: "Lady Olynder, Mortarch of Grief",
+    faction: "Nighthaunt",
+	aliases: [
+      "lady olynder, mortarch of grief",
+    ],
+  },
+  {
+    name: "Lord Executioner",
+    faction: "Nighthaunt",
+	aliases: [
+      "lord executioner",
+    ],
+  },
+  {
+    name: "Lord Vitriolic",
+    faction: "Nighthaunt",
+	aliases: [
+      "lord vitriolic",
+    ],
+  },
+  {
+    name: "Nagash, Supreme Lord of the Undead",
+    faction: "Nighthaunt",
+	aliases: [
+      "nagash, supreme lord of the undead",
+    ],
+  },
+  {
+    name: "Reikenor the Grimhailer",
+    faction: "Nighthaunt",
+	aliases: [
+      "reikenor the grimhailer",
+    ],
+  },
+  {
+    name: "Scriptor Mortis",
+    faction: "Nighthaunt",
+	aliases: [
+      "scriptor mortis",
+    ],
+  },
+  {
+    name: "Spirit Torment",
+    faction: "Nighthaunt",
+	aliases: [
+      "spirit torment",
+    ],
+  },
+  {
+    name: "Bladegheist Revenants",
+    faction: "Nighthaunt",
+	aliases: [
+      "bladegheist revenants",
+    ],
+  },
+  {
+    name: "Chainghasts",
+    faction: "Nighthaunt",
+	aliases: [
+      "chainghasts",
+    ],
+  },
+  {
+    name: "Chainrasps",
+    faction: "Nighthaunt",
+	aliases: [
+      "chainrasps",
+    ],
+  },
+  {
+    name: "Craventhrone Guard",
+    faction: "Nighthaunt",
+	aliases: [
+      "craventhrone guard",
+    ],
+  },
+  {
+    name: "Dreadscythe Harridans",
+    faction: "Nighthaunt",
+	aliases: [
+      "dreadscythe harridans",
+    ],
+  },
+  {
+    name: "Grimghast Reapers",
+    faction: "Nighthaunt",
+	aliases: [
+      "grimghast reapers",
+    ],
+  },
+  {
+    name: "Myrmourn Banshees",
+    faction: "Nighthaunt",
+	aliases: [
+      "myrmourn banshees",
+    ],
+  },
+  {
+    name: "Pox-Wretches",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "pox-wretches",
+    ],
+  },
+  {
+    name: "Pyregheists",
+    faction: "Nighthaunt",
+	aliases: [
+      "pyregheists",
+    ],
+  },
+  {
+    name: "Spirit Hosts",
+    faction: "Nighthaunt",
+	aliases: [
+      "spirit hosts",
+    ],
+  },
+  {
+    name: "Dreadblade Harrows",
+    faction: "Nighthaunt",
+	aliases: [
+      "dreadblade harrows",
+    ],
+  },
+  {
+    name: "Hexwraiths",
+    faction: "Nighthaunt",
+	aliases: [
+      "hexwraiths",
+    ],
+  },
+  {
+    name: "Black Coach",
+    faction: "Nighthaunt",
+	aliases: [
+      "black coach",
+    ],
+  },
+  {
+    name: "Nexus of Grief",
+    faction: "Nighthaunt",
+	aliases: [
+      "nexus of grief",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default NIGHTHAUNT;
