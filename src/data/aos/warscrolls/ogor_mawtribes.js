9cd78ca89e215e1704294711b8a556ac5d7c2818
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-09 23:46:02 +0000

    Create ogor_mawtribes.js

---
+++ src/data/aos/warscrolls/ogor_mawtribes.js
@@ -0,0 +1,227 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — OGOR MAWTRIBES
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const OGOR_MAWTRIBES = [
+    {
+    name: "Bloodpelt Hunter",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "bloodpelt hunter",
+    ],
+  },
+  {
+    name: "Tomb Banshee",
+    faction: "Nighthaunt",
+	aliases: [
+      "tomb banshee",
+    ],
+  },
+  {
+    name: "Butcher",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "butcher",
+    ],
+  },
+   {
+    name: "Firebelly",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "firebelly",
+    ],
+  },
+   {
+    name: "Frostlord on Stonehorn",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "frostlord on stonehorn",
+    ],
+  },
+  {
+    name: "Frostlord on Thundertusk",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "frostlord on thundertusk",
+    ],
+  },
+  {
+    name: "Huskard on Stonehorn",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "huskard on stonehorn",
+    ],
+  },
+  {
+    name: "Huskard on Thundertusk",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "huskard on thundertusk",
+    ],
+  },
+  {
+    name: "Icebrow Hunter",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "icebrow hunter",
+    ],
+  },
+  {
+    name: "Kragnos, the End of Empires",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "kragnos, the end of empires",
+    ],
+  },
+  {
+    name: "Mantrapper",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "mantrapper",
+    ],
+  },
+  {
+    name: "Slaughtermaster",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "slaughtermaster",
+    ],
+  },
+  {
+    name: "Tyrant",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "tyrant",
+    ],
+  },
+  {
+    name: "Gnoblars",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "gnoblars",
+    ],
+  },
+  {
+    name: "Gorger Mawpack",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "gorger mawpack",
+    ],
+  },
+  {
+    name: "Icefall Yhetees",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "icefall yhetees",
+    ],
+  },
+  {
+    name: "Ironguts",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "ironguts",
+    ],
+  },
+  {
+    name: "Leadbelchers",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "leadbelchers",
+    ],
+  },
+  {
+    name: "Maneaters",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "maneaters",
+    ],
+  },
+  {
+    name: "Ogor Gluttons",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "ogor gluttons",
+    ],
+  },
+  {
+    name: "Mournfang Pack",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "mournfang pack",
+    ],
+  },
+  {
+    name: "Frost Sabres",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "frost sabres",
+    ],
+  },
+  {
+    name: "Stonehorn Beastriders",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "stonehorn beastriders",
+    ],
+  },
+  {
+    name: "Thundertusk Beastriders",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "thundertusk beastriders",
+    ],
+  },
+  {
+    name: "Gnoblar Scraplauncher",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "gnoblar sraplauncher",
+    ],
+  },
+  {
+    name: "Ironblaster",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "ironblaster",
+    ],
+  },
+  {
+    name: "Great Mawpot",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "great mawpot",
+    ],
+  },
+  {
+    name: "Mawpit",
+    faction: "Ogor Mawtribes",
+	aliases: [
+      "mawpit",
+    ],
+  },
+	{
+    name: "Scourge of Ghyran Gnoblar Scraplauncher",
+    faction: "Nighthaunt",
+	aliases: [
+      "scourge of ghyran gnoblar scraplauncher",
+    ],
+  },
+	{
+    name: "Scourge of Ghyran Ironblaster",
+    faction: "Nighthaunt",
+	aliases: [
+      "scourge of ghyran ironblaster",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default OGOR_MAWTRIBES;
