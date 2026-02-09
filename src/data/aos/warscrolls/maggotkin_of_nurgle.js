f47b1faf031e5b9fe2e7ba81abbc180b4625c76d
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-09 16:55:56 +0000

    Create maggotkin_of_nurgle.js

---
+++ src/data/aos/warscrolls/maggotkin_of_nurgle.js
@@ -0,0 +1,255 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — MAGGOTKIN OF NURGLE
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const MAGGOTKIN_OF_NURGLE = [
+    {
+    name: "Belga the Cystwitch",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "belga the cystwitch",
+    ],
+  },
+  {
+    name: "Bloab Rotspawned",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "bloab rotspawned",
+    ],
+  },
+  {
+    name: "Cankerborn",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "cankerborn",
+    ],
+  },
+   {
+    name: "Festus the Leechlord",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "festus the leechlord",
+    ],
+  },
+   {
+    name: "Gelgus Pust",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "gelgus pust",
+    ],
+  },
+  {
+    name: "Great Unclean One",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "great unclean one",
+    ],
+  },
+  {
+    name: "Gutrot Spume",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "gutrot spume",
+    ],
+  },
+  {
+    name: "Harbringer of Decay",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "harbringer of decay",
+    ],
+  },
+  {
+    name: "Horticulous Slimux",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "horticulous slimux",
+    ],
+  },
+  {
+    name: "Lord of Afflictions",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "lord of afflictions",
+    ],
+  },
+  {
+    name: "Lord of Blights",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "lord of blights",
+    ],
+  },
+  {
+    name: "Lord of Plagues",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "lord of plagues",
+    ],
+  },
+  {
+    name: "Morbidex Twiceborn",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "morbidex twiceborn",
+    ],
+  },
+  {
+    name: "Orghotts Daemonspew",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "orghotts daemonspew",
+    ],
+  },
+  {
+    name: "Poxbringer, Herald of Nurgle",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "poxbringer, herald of nurgle",
+    ],
+  },
+  {
+    name: "Robringer Sorcerer",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "rotbringer sorcerer",
+    ],
+  },
+  {
+    name: "Rotigus",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "rotigus",
+    ],
+  },
+  {
+    name: "Shaman Foulhoof",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "shaman foulhoof",
+    ],
+  },
+  {
+    name: "Sloppity Bilepiper, Herald of Nurgle",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "sloppity bilepiper, herald of nurgle",
+    ],
+  },
+  {
+    name: "Spoilpox Scrivener, Herald of Nurgle",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "spoilpox scrivener, herald of nurgle",
+    ],
+  },
+  {
+    name: "The Glottkin",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "the glottkin",
+    ],
+  },
+  {
+    name: "Pestigors",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "pestigors",
+    ],
+  },
+  {
+    name: "Plaguebearers",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "plaguebearers",
+    ],
+  },
+  {
+    name: "Pox-Wretches",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "pox-wretches",
+    ],
+  },
+  {
+    name: "Putrid Blightkings",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "putrid blightkings",
+    ],
+  },
+  {
+    name: "Rotmire Creed",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "rotmire creed",
+    ],
+  },
+  {
+    name: "Rotswords",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "rotswords",
+    ],
+  },
+  {
+    name: "Plague Drones",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "plague drones",
+    ],
+  },
+  {
+    name: "Pusgoyle Blightlords",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "pusgoyle blightlords",
+    ],
+  },
+  {
+    name: "Sloven Knights",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "sloven knights",
+    ],
+  },
+  {
+    name: "Beast of Nurgle",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "beast of nurgle",
+    ],
+  },
+  {
+    name: "Nurglings",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "nurglings",
+    ],
+  },
+  {
+    name: "Feculent Gnarlmaw",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "feculent gnarlmaw",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Rotigus",
+    faction: "Maggotkin of Nurgle",
+	aliases: [
+      "scourge of ghyran rotigus",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default MAGGOTKIN_OF_NURGLE;
