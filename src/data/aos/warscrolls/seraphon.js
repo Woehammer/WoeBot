ba1dbc5179b7f6d66c9f4275aadf3ca1bd1a04be
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-10 23:34:40 +0000

    Create seraphon.js

---
+++ src/data/aos/warscrolls/seraphon.js
@@ -0,0 +1,255 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — SERAPHON
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const SERAPHON = [
+    {
+    name: "Lord Kroak",
+    faction: "Seraphon",
+	aliases: [
+      "lord kroak",
+    ],
+  },
+  {
+    name: "Ripperdactyl Chief",
+    faction: "Seraphon",
+	aliases: [
+      "ripperdactyl chief",
+    ],
+  },
+  {
+    name: "Saurus Astrolith Bearer",
+    faction: "Seraphon",
+	aliases: [
+      "saurus astrolith bearer",
+    ],
+  },
+   {
+    name: "Saurus Oldblood",
+    faction: "Seraphon",
+	aliases: [
+      "saurus oldblood",
+    ],
+  },
+   {
+    name: "Saurus Oldblood on Carnosaur",
+    faction: "Seraphon",
+	aliases: [
+      "saurus oldblood on carnosaur",
+    ],
+  },
+  {
+    name: "Saurus Scar-Veteran on Aggradon",
+    faction: "Seraphon",
+	aliases: [
+      "saurus scar-veteran on aggradon",
+    ],
+  },
+  {
+    name: "Saurus Scar-Veteran on Carnosaur",
+    faction: "Seraphon",
+	aliases: [
+      "saurus scar-veteran on carnosaur",
+    ],
+  },
+  {
+    name: "Skink Oracle on Troglodon",
+    faction: "Seraphon",
+	aliases: [
+      "skink oracle on troglodon,
+    ],
+  },
+  {
+    name: "Skink Starpriest",
+    faction: "Seraphon",
+	aliases: [
+      "skink starpriest",
+    ],
+  },
+  {
+    name: "Skink Starseer",
+    faction: "Seraphon",
+	aliases: [
+      "skink starseer",
+    ],
+  },
+  {
+    name: "Slann Starmaster",
+    faction: "Seraphon",
+	aliases: [
+      "slann starmaster",
+    ],
+  },
+  {
+    name: "Stegadon Chief",
+    faction: "Seraphon",
+	aliases: [
+      "stegadon chief",
+    ],
+  },
+  {
+    name: "Sunblood Pack",
+    faction: "Seraphon",
+	aliases: [
+      "sunblood pack",
+    ],
+  },
+  {
+    name: "Terradon Chief",
+    faction: "Seraphon",
+	aliases: [
+      "terradon chief",
+    ],
+  },
+  {
+    name: "Hunters of Huanchi with Dartpipes",
+    faction: "Seraphon",
+	aliases: [
+      "hunters of huanchi with dartpipes",
+    ],
+  },
+  {
+    name: "Hunters of Huanchi with Starstone Bolas",
+    faction: "Seraphon",
+	aliases: [
+      "hunters of huanchi with starstone bolas",
+    ],
+  },
+  {
+    name: "Kroxigor",
+    faction: "Seraphon",
+	aliases: [
+      "kroxigor",
+    ],
+  },
+  {
+    name: "Kroxigor Warspawned",
+    faction: "Seraphon",
+	aliases: [
+      "kroxigor warspawned",
+    ],
+  },
+  {
+    name: "Saurus Guard",
+    faction: "Seraphon",
+	aliases: [
+      "saurus guard",
+    ],
+  },
+  {
+    name: "Saurus Warriors",
+    faction: "Seraphon",
+	aliases: [
+      "saurus warriors",
+    ],
+  },
+  {
+    name: "Skinks",
+    faction: "Seraphon",
+	aliases: [
+      "skinks",
+    ],
+  },
+  {
+    name: "Aggradon Lancers",
+    faction: "Seraphon",
+	aliases: [
+      "aggradon lancers",
+    ],
+  },
+  {
+    name: "Raptadon Chargers",
+    faction: "Seraphon",
+	aliases: [
+      "raptadon chargers",
+    ],
+  },
+  {
+    name: "Raptadon Hunters",
+    faction: "Seraphon",
+	aliases: [
+      "raptadon hunters",
+    ],
+  },
+  {
+    name: "Ripperdactyl Riders",
+    faction: "Seraphon",
+	aliases: [
+      "ripperdactyl riders",
+    ],
+  },
+  {
+    name: "Terradon Riders",
+    faction: "Seraphon",
+	aliases: [
+      "terradon riders",
+    ],
+  },
+  {
+    name: "Spawn of Chotec",
+    faction: "Seraphon",
+	aliases: [
+      "spawn of chotec",
+    ],
+  },
+  {
+    name: "Terrawings",
+    faction: "Seraphon",
+	aliases: [
+      "terrawings",
+    ],
+  },
+  {
+    name: "Bastiladon with Ark of Sotek",
+    faction: "Seraphon",
+	aliases: [
+      "bastiladon with ark of sotek",
+    ],
+  },
+  {
+    name: "Bastiladon with Solar Engine",
+    faction: "Seraphon",
+	aliases: [
+      "bastiladon with solar engine",
+    ],
+  },
+  {
+    name: "Engine of the Gods",
+    faction: "Seraphon",
+	aliases: [
+      "engine of the gods",
+    ],
+  },
+  {
+    name: "Stegadon",
+    faction: "Seraphon",
+	aliases: [
+      "stegadon",
+    ],
+  },
+  {
+    name: "Realmshaper Engine",
+    faction: "Seraphon",
+	aliases: [
+      "realmshaper engine",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Saurus Oldblood on Carnosaur",
+    faction: "Seraphon",
+	aliases: [
+      "scourge of ghyran saurus oldblood on carnosaur",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default SERAPHON;
