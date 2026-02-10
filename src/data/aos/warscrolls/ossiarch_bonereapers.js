a60c2984c33eb2bae24f753853d2e8b018a28271
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-10 22:50:49 +0000

    Create ossiarch_bonereapers.js

---
+++ src/data/aos/warscrolls/ossiarch_bonereapers.js
@@ -0,0 +1,171 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — OSSIARCH BONEREAPERS
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const OSSIARCH_BONEREAPERS = [
+    {
+    name: "Arch-Kavalos Zandtos",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "arch-kavalos zandtos",
+    ],
+  },
+  {
+    name: "Arkhan the Black, Mortarch of Sacrament",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "arkhan the black, mortarch of sacrament",
+    ],
+  },
+  {
+    name: "Katakros, Mortarch of the Necropolis",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "katakros, mortarch of the necropolis",
+    ],
+  },
+   {
+    name: "Liege-Kavalos",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "liege-kavalos",
+    ],
+  },
+   {
+    name: "Mortisan Boneshaper",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "mortisan boneshaper",
+    ],
+  },
+  {
+    name: "Mortisan Ossifector",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "mortisan ossifector",
+    ],
+  },
+  {
+    name: "Mortisan Soulmason",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "mortisan soulmason",
+    ],
+  },
+  {
+    name: "Mortisan Soulreaper",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "mortisan soulreaper",
+    ],
+  },
+  {
+    name: "Nagash, Supreme Lord of the Undead",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "nagash, supreme lord of the undead",
+    ],
+  },
+  {
+    name: "Vokmortian, Master of the Bone-tithe",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "vokmortian, master of the bone-tithe",
+    ],
+  },
+  {
+    name: "Immortis Guard",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "immortis guard",
+    ],
+  },
+  {
+    name: "Morghast Archai",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "morghast archai",
+    ],
+  },
+  {
+    name: "Morghast Harbringers",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "morghast harbringers",
+    ],
+  },
+  {
+    name: "Mortek Guard",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "mortek guard",
+    ],
+  },
+  {
+    name: "Necropolis Stalkers",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "necropolis stalkers",
+    ],
+  },
+  {
+    name: "Teratic Cohort",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "teratic cohort",
+    ],
+  },
+  {
+    name: "Kavalos Deathriders",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "kavalos deathriders",
+    ],
+  },
+  {
+    name: "Gothizzar Harvester",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "gothizzar harvester",
+    ],
+  },
+  {
+    name: "Mortek Crawler",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "mortek crawler",
+    ],
+  },
+  {
+    name: "Bone-tithe Nexus",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "bone-tithe nexus",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Mortisan Boneshaper",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "scourge of ghyran mortisan boneshaper",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Mortek Guard",
+    faction: "Ossiarch Bonereapers",
+	aliases: [
+      "scourge of ghyran mortek guard",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default OSSIARCH_BONEREAPERS;
