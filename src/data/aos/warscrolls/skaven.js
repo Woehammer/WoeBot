bb0cb2d206b378e95a93634b14d2251f18e0558c
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-11 00:07:46 +0000

    Create skaven.js

---
+++ src/data/aos/warscrolls/skaven.js
@@ -0,0 +1,297 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — SKAVEN
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const SKAVEN = [
+    {
+    name: "Arch-Warlock",
+    faction: "Skaven",
+	aliases: [
+      "arch-warlock",
+    ],
+  },
+  {
+    name: "Clawlord",
+    faction: "Skaven",
+	aliases: [
+      "clawlord",
+    ],
+  },
+  {
+    name: "Clawlord on Gnaw-beast",
+    faction: "Skaven",
+	aliases: [
+      "clawlord on gnaw-beast",
+    ],
+  },
+   {
+    name: "Deathmaster",
+    faction: "Skaven",
+	aliases: [
+      "deathmaster",
+    ],
+  },
+   {
+    name: "Grey Seer",
+    faction: "Skaven",
+	aliases: [
+      "grey seer",
+    ],
+  },
+  {
+    name: "Grey Seer on Screaming Bell",
+    faction: "Skaven",
+	aliases: [
+      "grey seer on screaming bell",
+    ],
+  },
+  {
+    name: "Krittok Foulblade",
+    faction: "Skaven",
+	aliases: [
+      "krittok foulblade",
+    ],
+  },
+  {
+    name: "Lord Skreech Verminking",
+    faction: "Skaven",
+	aliases: [
+      "lord skreech verminking",
+    ],
+  },
+  {
+    name: "Master Moulder",
+    faction: "Skaven",
+	aliases: [
+      "master moulder",
+    ],
+  },
+  {
+    name: "Plague Priest on Plague Furnace",
+    faction: "Skaven",
+	aliases: [
+      "plague priest on plague furnace",
+    ],
+  },
+  {
+    name: "Thanquol on Boneripper",
+    faction: "Skaven",
+	aliases: [
+      "thanquol on boneripper",
+    ],
+  },
+  {
+    name: "Verminlord Corruptor",
+    faction: "Skaven",
+	aliases: [
+      "verminlord corruptor",
+    ],
+  },
+  {
+    name: "Verminlord Deceiver",
+    faction: "Skaven",
+	aliases: [
+      "verminlord deceiver",
+    ],
+  },
+  {
+    name: "Verminlord Warbringer",
+    faction: "Skaven",
+	aliases: [
+      "verminlord warbringer",
+    ],
+  },
+  {
+    name: "Verminlord Warpseer",
+    faction: "Skaven",
+	aliases: [
+      "verminlord warpseer",
+    ],
+  },
+  {
+    name: "Vizzik Skour, Prophet of the Horned Rat",
+    faction: "Skaven",
+	aliases: [
+      "vizzik skour, prophet of the horned rat",
+    ],
+  },
+  {
+    name: "Warlock Bombardier",
+    faction: "Skaven",
+	aliases: [
+      "warlock bombardier",
+    ],
+  },
+  {
+    name: "Warlock Engineer",
+    faction: "Skaven",
+	aliases: [
+      "warlock engineer",
+    ],
+  },
+  {
+    name: "Warlock Galvaneer",
+    faction: "Skaven",
+	aliases: [
+      "warlock galvaneer",
+    ],
+  },
+  {
+    name: "Acolyte Globadiers",
+    faction: "Skaven",
+	aliases: [
+      "acolyte globadiers",
+    ],
+  },
+  {
+    name: "Clanrats",
+    faction: "Skaven",
+	aliases: [
+      "clanrats",
+    ],
+  },
+  {
+    name: "Night Runners",
+    faction: "Skaven",
+	aliases: [
+      "night runners",
+    ],
+  },
+  {
+    name: "Plague Monks",
+    faction: "Skaven",
+	aliases: [
+      "plague monks",
+    ],
+  },
+  {
+    name: "Plaguepack",
+    faction: "Skaven",
+	aliases: [
+      "plaguepack",
+    ],
+  },
+  {
+    name: "Rat Ogors",
+    faction: "Skaven",
+	aliases: [
+      "rat ogors",
+    ],
+  },
+  {
+    name: "Ratling Guns",
+    faction: "Skaven",
+	aliases: [
+      "ratling guns",
+    ],
+  },
+  {
+    name: "Stormfiends",
+    faction: "Skaven",
+	aliases: [
+      "stormfiends",
+    ],
+  },
+  {
+    name: "Stormvermin",
+    faction: "Skaven",
+	aliases: [
+      "stormvermin",
+    ],
+  },
+  {
+    name: "Warpfire Throwers",
+    faction: "Skaven",
+	aliases: [
+      "warpfire throwers",
+    ],
+  },
+  {
+    name: "Warplock Jezzails",
+    faction: "Skaven",
+	aliases: [
+      "warplock jezzails",
+    ],
+  },
+  {
+    name: "Warpvolt Scourgers",
+    faction: "Skaven",
+	aliases: [
+      "warpvolt scourgers",
+    ],
+  },
+  {
+    name: "Brood Terror",
+    faction: "Skaven",
+	aliases: [
+      "brood terror",
+    ],
+  },
+  {
+    name: "Hell Pit Abomination",
+    faction: "Skaven",
+	aliases: [
+      "hell pit abomination",
+    ],
+  },
+  {
+    name: "Doom-Flayers",
+    faction: "Skaven",
+	aliases: [
+      "doom-flayers",
+    ],
+  },
+  {
+    name: "Doomwheel",
+    faction: "Skaven",
+	aliases: [
+      "doomwheel",
+    ],
+  },
+  {
+    name: "Plagueclaw",
+    faction: "Skaven",
+	aliases: [
+      "plagueclaw",
+    ],
+  },
+  {
+    name: "Ratling Warpblaster",
+    faction: "Skaven",
+	aliases: [
+      "ratling warpblaster",
+    ],
+  },
+  {
+    name: "Warp Lightning Cannon",
+    faction: "Skaven",
+	aliases: [
+      "warp lightning cannon",
+    ],
+  },
+  {
+    name: "Warp-Grinder",
+    faction: "Skaven",
+	aliases: [
+      "warp-grinder",
+    ],
+  },
+  {
+    name: "Gnawhole",
+    faction: "Skaven",
+	aliases: [
+      "gnawhole",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default SKAVEN;
