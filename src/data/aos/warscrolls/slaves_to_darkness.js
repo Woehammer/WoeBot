950785dac26f5022e997beddc02f171e705a3bdb
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-11 14:22:54 +0000

    Create slaves_to_darkness.js

---
+++ src/data/aos/warscrolls/slaves_to_darkness.js
@@ -0,0 +1,359 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — SLAVES TO DARKNESS
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const SLAVES_TO_DARKNESS = [
+    {
+    name: "Abraxia, Spear of the Everchosen",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "abraxia, spear of the everchosen",
+    ],
+  },
+  {
+    name: "Archaon, the Everchosen",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "archaon, the everchosen",
+    ],
+  },
+  {
+    name: "Be'lakor, the Dark Master",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "be'lakor, the dark master",
+    ],
+  },
+   {
+    name: "Centuarion Marshal",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "centaurion marshal",
+    ],
+  },
+   {
+    name: "Chaos Lord",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos lord",
+    ],
+  },
+  {
+    name: "Chaos Lord on Daemonic Mount",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos lord on daemonic mount",
+    ],
+  },
+  {
+    name: "Chaos Lord on Karkadrak",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos lord on karkadrak",
+    ],
+  },
+  {
+    name: "Chaos Sorcerer Lord",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos sorcerer lord",
+    ],
+  },
+  {
+    name: "Daemon Prince",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "daemon prince",
+    ],
+  },
+  {
+    name: "Darkoath Chieftain",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath chieftain",
+    ],
+  },
+  {
+    name: "Darkoath Chieftain on Warsteed",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath chieftain on warsteed",
+    ],
+  },
+  {
+    name: "Darkoath Warqueen",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath warqueen",
+    ],
+  },
+  {
+    name: "Eternus, Blade of the First Prince",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "eternus, blade of the first prince",
+    ],
+  },
+  {
+    name: "Exalted Hero of Chaos",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "exalted hero of chaos",
+    ],
+  },
+  {
+    name: "Gaunt Summoner",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "gaunt summoner",
+    ],
+  },
+  {
+    name: "Gaunt Summoner on Disc of Tzeentch",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "gaunt summoner on disc of tzeentch",
+    ],
+  },
+  {
+    name: "Gunnar Brand",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "gunnar brand",
+    ],
+  },
+  {
+    name: "Ogroid Myrmidon",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "ogroid myrmidon",
+    ],
+  },
+  {
+    name: "Singri Brand",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "singri brand",
+    ],
+  },
+  {
+    name: "Chaos Chosen",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos chosen",
+    ],
+  },
+  {
+    name: "Chaos Furies",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos furies",
+    ],
+  },
+  {
+    name: "Chaos Legionnaires",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos legionnaires",
+    ],
+  },
+  {
+    name: "Chaos Warriors",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos warriors",
+    ],
+  },
+  {
+    name: "Darkoath Marauders",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath marauders",
+    ],
+  },
+  {
+    name: "Darkoath Savagers",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath savagers",
+    ],
+  },
+  {
+    name: "Ogroid Theridons",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "ogroid theridons",
+    ],
+  },
+  {
+    name: "The Oathsworn Kin",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "the oathsworn kin",
+    ],
+  },
+  {
+    name: "Chaos Knights",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos knights",
+    ],
+  },
+  {
+    name: "Darkoath Fellriders",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath fellriders",
+    ],
+  },
+  {
+    name: "Varanguard",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "varanguard",
+    ],
+  },
+  {
+    name: "Chaos Spawn",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos spawn",
+    ],
+  },
+  {
+    name: "Darkoath Wilderfiend",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "darkoath wilderfiend",
+    ],
+  },
+  {
+    name: "Raptoryx",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "raptoryx",
+    ],
+  },
+  {
+    name: "Fomoroid Crusher",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "fomoroid crusher",
+    ],
+  },{
+    name: "Mindstealer Sphiranx",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "mindstealer sphiranx",
+    ],
+  },
+  {
+    name: "Mutalith Vortex Beast",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "mutalith vortex beast",
+    ],
+	{
+    name: "Slaughterbrute",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "slaughterbrute",
+    ],
+  },
+  {
+    name: "Chaos Chariot",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "chaos chariot",
+    ],
+  },
+  {
+    name: "Gorebeast Chariot",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "gorebeast chariot",
+    ],
+  },
+  {
+    name: "Nexus Chaotica",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "nexus chaotica",
+    ],
+  },
+  },
+  {
+    name: "Scourge of Ghyran Abraxia, Spear of the Everchosen",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "scourge of ghyran abraxia, spear of the everchosen",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Bloodletters",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince bloodletters",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Flamers of Tzeentch",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince flamers of tzeentch",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Plaguebearers",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince plaguebearers",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Bloodcrushers",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince bloodcrushers",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Beasts of Nurgle",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince beasts of  nurgle",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Fiends",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince fiends",
+    ],
+  },
+  {
+    name: "Legion of the First Screamers of Tzeentch",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince screamers of tzeentch",
+    ],
+  },
+  {
+    name: "Legion of the First Prince Hellflayer",
+    faction: "Slaves to Darkness",
+	aliases: [
+      "legion of the first prince hellflayer",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default SLAVES_TO_DARKNESS;
