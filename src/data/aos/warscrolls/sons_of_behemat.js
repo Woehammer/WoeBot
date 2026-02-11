2a8593052f5548163a2e4cc73ebd0c71c296fb5e
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-11 15:19:23 +0000

    Create sons_of_behemat.js

---
+++ src/data/aos/warscrolls/sons_of_behemat.js
@@ -0,0 +1,80 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — SONS OF BEHEMAT
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const SONS_OF_BEHEMAT = [
+    {
+    name: "Beast-smasher Mega-Gargant",
+    faction: "Sons of Behemat",
+	aliases: [
+      "beast-smasher mega-gargant",
+    ],
+  },
+  {
+    name: "Gatebreaker Mega-Gargant",
+    faction: "Sons of Behemat",
+	aliases: [
+      "gatebreaker mega-gargant",
+    ],
+  },
+  {
+    name: "King Brodd",
+    faction: "Sons of Behemat",
+	aliases: [
+      "king brodd",
+    ],
+  },
+   {
+    name: "Kragnos, the End of Empires",
+    faction: "Sons of Behemat",
+	aliases: [
+      "kragnos, the end of empires",
+    ],
+  },
+   {
+    name: "Kraken-eater Mega-Gargant",
+    faction: "Sons of Behemat",
+	aliases: [
+      "kraken-eater mega-gargant",
+    ],
+  },
+  {
+    name: "Warstomper Mega-Gargant",
+    faction: "Sons of Behemat",
+	aliases: [
+      "warstomper mega-gargant",
+    ],
+  },
+  {
+    name: "Mancrusher Gargant",
+    faction: "Sons of Behemat",
+	aliases: [
+      "mancrusher gargant",
+    ],
+  },
+  {
+    name: "Mancrusher Mob",
+    faction: "Sons of Behemat",
+	aliases: [
+      "mancrusher mob",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Kraken-eater Mega-Gargant",
+    faction: "Sons of Behemat",
+	aliases: [
+      "scourge of ghyran kraken-eater mega-gargant",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default SONS_OF_BEHEMAT;
