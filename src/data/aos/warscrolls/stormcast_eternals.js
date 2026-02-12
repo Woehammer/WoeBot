2bff4897659771e3eb20cbe5f1a65647de623525
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-12 15:07:58 +0000

    Create stormcast_eternals.js

---
+++ src/data/aos/warscrolls/stormcast_eternals.js
@@ -0,0 +1,422 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — STORMCAST ETERNALS
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const STORMCAST_ETERNALS = [
+    {
+    name: "Celestant-Prime, Hammer of Sigmar",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "celestant-prime, hammer of sigmar",
+    ],
+  },
+  {
+    name: "Drakesworn Templar",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "drakesworn templar",
+    ],
+  },
+  {
+    name: "Gardus Steel Soul",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "gardus steel soul",
+    ],
+  },
+   {
+    name: "Ionus Cryptborn, Warden of Lost Souls",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "ionus cryptborn, warden of lost souls",
+  },
+   {
+    name: "Iridan the Witness",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "iridan the witness",
+    ],
+  },
+  {
+    name: "Karazai the Scarred",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "karazai the scarred",
+    ],
+  },
+  {
+    name: "Knight-Arcanum",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-arcanum",
+    ],
+  },
+  {
+    name: "Knight-Azyros",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-azyros",
+    ],
+  },
+  {
+    name: "Knight-Draconis",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-draconis",
+    ],
+  },
+  {
+    name: "Knight-Judicator with Gryph-hounds",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-judictor with gryph-hounds",
+    ],
+  },
+  {
+    name: "Knight-Questor",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-questor",
+    ],
+  },
+  {
+    name: "Knight-Relictor",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-relictor",
+    ],
+  },
+  {
+    name: "Knight-Vexillor",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "knight-vexillor",
+    ],
+  },
+  {
+    name: "Krondys, Son of Dracothion",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "krondys, son of dracothion",
+    ],
+  },
+  {
+    name: "Lorai, Child of the Abyss",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lorai, child of the abyss",
+    ],
+  },
+  {
+    name: "Lord-Aquilor",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-aquilor",
+    ],
+  },
+  {
+    name: "Lord-Celestant",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-celestant",
+    ],
+  },
+  {
+    name: "Lord-Celestant on Dracoth",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-celestant on dracoth",
+    ],
+  },
+  {
+    name: "Lord-Celestant on Stardrake",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-celestant on stardrake",
+    ],
+  },
+  {
+    name: "Lord-Commander Bastian Carthalos",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-commander bastian carthalos",
+    ],
+  },
+  {
+    name: "Lord-Imperatant",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-imperatant",
+    ],
+  },
+  {
+    name: "Lord-Relictor",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-relictor",
+    ],
+  },
+  {
+    name: "Lord-Terminos",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-terminos",
+    ],
+  },
+  {
+    name: "Lord-Veritant",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-veritant",
+    ],
+  },
+  {
+    name: "Lord-Vigilant on Gryph-Stalker",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-vigilant on gryph-stalker",
+    ],
+  },
+  {
+    name: "Lord-Vigilant on Morrgryph",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "lord-vigilant on morrgryph",
+    ],
+  },
+  {
+    name: "Neave Blacktalon",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "neave blacktalon",
+    ],
+  },
+  {
+    name: "Tornus the Redeemed",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "tornus the redeemed",
+    ],
+  },
+  {
+    name: "Vandus Hammerhand",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vandus hammerhand",
+    ],
+  },
+  {
+    name: "Yndrasta, the Celestial Spear",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "yndrasta, the celestial spear",
+    ],
+  },
+  {
+    name: "Annihilators",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "annihilators",
+    ],
+  },
+  {
+    name: "Annihilators with Meteoric Grandhammers",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "annihilators with meteoric grandhammers",
+    ],
+  },
+  {
+    name: "Liberators",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "liberators",
+    ],
+  },
+  {
+    name: "Neave's Companions",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "neave's companions",
+    ],
+  },
+  {
+    name: "Praetors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "praetors",
+    ],
+  },
+  {
+    name: "Prosecutors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "prosecutors",
+    ],
+  },
+  {
+    name: "Questor Soulsworn",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "questor soulsworn",
+    ],
+  },
+  {
+    name: "Reclusians",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "reclusians",
+    ],
+  },
+  {
+    name: "Stormcoven",
+    faction: "Stormcast Eternals",
+	aliases: [
+      stormcoven",
+    ],
+  },
+  {
+    name: "Vanguard-Hunters",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vanguard-hunters",
+    ],
+  },
+  {
+    name: "Vanguard-Raptors with Hurricane Crossbows",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vanguard-raptors with hurricane crossbows",
+    ],
+  },
+  {
+    name: "Vanguard-Raptors with Longstrike Crossbows",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vanguard-raptors with longstrike crossbows",
+    ],
+  },
+  {
+    name: "Vanquishers",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vanquishers",
+    ],
+  },
+  {
+    name: "Vigilors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vigilors",
+    ],
+  },
+  {
+    name: "Vindictors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vindictors",
+    ],
+  },
+  {
+    name: "Dracothian Guard Concussors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "dracothian guard concussors",
+    ],
+  },
+  {
+    name: "Dracothian Guard Desolators",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "dracothian guard desolators",
+    ],
+  },
+  {
+    name: "Dracothian Guard Fulminators",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "dracothian guard fulminators",
+    ],
+  },
+  {
+    name: "Dracothian Guard Tempestors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "dracothian guard tempestors",
+    ],
+  },
+  {
+    name: "Stormstrike Palladors",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "stormstrike palladors",
+    ],
+  },
+  {
+    name: "Vanguard-Palladors with Shock Handaxes",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vanguard-palladors with shock handaxes",
+    ],
+  },
+  {
+    name: "Vanguard-Palladors with Starstrike Javelins",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "vanguard-palladors with starstrike javelins",
+    ],
+  },
+  {
+    name: "Aetherwings",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "aetherwings",
+    ],
+  },
+  {
+    name: "Gryph-hounds",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "gryph-hounds",
+    ],
+  },
+  {
+    name: "Stormdrake Guard",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "stormdrake guard",
+    ],
+  },
+  {
+    name: "Stormstrike Chariot",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "stormstrike chariot",
+    ],
+  },
+  {
+    name: "Stormreach Portal",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "stormreach portal",
+    ],
+  },
+  {
+    name: "Scourge of Ghyran Iridan the Witness",
+    faction: "Stormcast Eternals",
+	aliases: [
+      "scourge of ghyran iridan the witness",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default STORMCAST_ETERNALS;
