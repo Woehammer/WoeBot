9f299b5cc81fe6a5961224edcd0315291fb06df8
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-12 09:19:13 +0000

    Create soulblight_gravelords.js

---
+++ src/data/aos/warscrolls/soulblight_gravelords.js
@@ -0,0 +1,325 @@
+// ==================================================
+// LOOKUP: AOS WARSCROLLS — SOULBLIGHT GRAVELORDS
+// PURPOSE: Canonical warscroll names + alias definitions
+// ==================================================
+
+// ==================================================
+// WARSCROLL DEFINITIONS
+// ==================================================
+
+const SOULBLIGHT_GRAVELORDS = [
+    {
+    name: "Bloodseeker Palanquin",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "bloodseeker palanquin",
+    ],
+  },
+  {
+    name: "Cado Ezechiar, The Hollow King",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "cado ezechiar, the hollow king",
+    ],
+  },
+  {
+    name: "Gorslav the Gravekeeper",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "gorslav the gravekeeper",
+    ],
+  },
+   {
+    name: "Torgillius the Chamberlain",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "torgillius the chamberlain",
+    ],
+  },
+   {
+    name: "Vampire Lord on Zombie Dragon",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vampire lord on zombie dragon",
+    ],
+  },
+  {
+    name: "Watch Captain Halgrim",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "watch captain halgrim",
+    ],
+  },
+  {
+    name: "Kosargi Nightguard",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "kosargi nightguard",
+    ],
+  },
+  {
+    name: "Vyrkos Blood-born",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vyrkos blood-born",
+    ],
+  },
+  {
+    name: "Vargskyr",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vargskyr",
+    ],
+  },
+  {
+    name: "Terrorgheist",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "terrorgheist",
+    ],
+  },
+  {
+    name: "Belladamma Volga, First of the Vyrkos",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "belladamma volga, first of the vyrkos",
+    ],
+  },
+  {
+    name: "Blades of the Hollow King",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "blades of the hollow king",
+    ],
+  },
+  {
+    name: "Coven Throne",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "coven throne",
+    ],
+  },
+  {
+    name: "Ivya Volga, The Outcast",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "ivya volga, the outcast",
+    ],
+  },
+  {
+    name: "Kritza, the Rat Prince",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "kritza, the rat prince",
+    ],
+  },
+  {
+    name: "Lady Annika, the Thirsting Blade",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "lady annika, the thirsting blade",
+    ],
+  },
+  {
+    name: "Lauka Vai, Mother of Nightmares",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "lauka vai, mother of nightmares",
+    ],
+  },
+  {
+    name: "Mannfred von Carstein, Mortarch of Night",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "mannfred von carstein, mortarch of night",
+    ],
+  },
+  {
+    name: "Mortis Engine",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "mortis engine",
+    ],
+  },
+  {
+    name: "Nagash, Supreme Lord of the Undead",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "nagash, supreme lord of the undead",
+    ],
+  },
+  {
+    name: "Necromancer",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "necromancer",
+    ],
+  },
+  {
+    name: "Neferata, Mortarch of Blood",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "neferata, mortarch of blood",
+    ],
+  },
+  {
+    name: "Prince Vhordrai",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "prince vhordrai",
+    ],
+  },
+  {
+    name: "Radukar the Beast",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "radukar the beast",
+    ],
+  },
+  {
+    name: "Radukar the Wolf",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "radukar the wolf",
+    ],
+  },
+  {
+    name: "Sekhar, Fang of Nulahmia",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "sekhar, fang of nulahmia",
+    ],
+  },
+  {
+    name: "Vampire Lord",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vampire lord",
+    ],
+  },
+  {
+    name: "Vampire Lord on Nightmare Steed",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vampire lord on nightmare steed",
+    ],
+  },
+  {
+    name: "Vengorian Lord",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vengorian lord",
+    ],
+  },
+  {
+    name: "Wight King",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "wight king",
+    ],
+  },
+  {
+    name: "Wight King on Skeletal Steed",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "wight king on skeletal steed",
+    ],
+  },
+  {
+    name: "Wight Lord on Skeletal Steed",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "wight lord on skeletal steed",
+    ],
+  },
+  {
+    name: "Askurgan Trueblades",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "askurgan trueblades",
+    ],
+  },
+  {
+    name: "Barrow Guard",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "barrow guard",
+    ],
+  },
+  {
+    name: "Deadwalker Zombies",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "deadwalker zombies",
+    ],
+  },
+  {
+    name: "Deathrattle Skeletons",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "deathrattle skeletons",
+    ],
+  },
+  {
+    name: "Vargheists",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "vargheists",
+    ],
+  },
+  {
+    name: "Barrow Knights",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "barrow knights",
+    ],
+  },
+  {
+    name: "Blood Knights",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "blood knights",
+    ],
+  },
+  {
+    name: "Dire Wolves",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "dire wolves",
+    ],
+  },
+  {
+    name: "Fell Bats",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "fell bats",
+    ],
+  },
+  {
+    name: "Revenant Draconith",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "revenant draconith",
+    ],
+  },
+  {
+    name: "Corpse Cart",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "corpse cart",
+    ],
+  },
+  {
+    name: "Cursed Sepulchre",
+    faction: "Soulblight Gravelords",
+	aliases: [
+      "cursed sepulchre",
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default SOULBLIGHT_GRAVELORDS;
