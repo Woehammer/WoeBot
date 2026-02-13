855f9ad037a7d9bc8faa7f83ee4076e269f4ffd5
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-13 16:30:20 +0000

    Create regiments_of_renown.js

---
+++ src/data/aos/regiments_of_renown.js
@@ -0,0 +1,527 @@
+// ==================================================
+// LOOKUP: AOS REGIMENTS OF RENOWN
+// PURPOSE: Canonical regiment names + aliases + faction eligibility
+// ==================================================
+
+// ==================================================
+// REGIMENT DEFINITIONS
+// ==================================================
+
+export const REGIMENTS_OF_RENOWN = [
+  {
+    name: "Big Drogg Fort-Kicka",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Ironjawz",
+	  "Kruleboyz",
+	  "Gloomspite Gitz",
+	  "Ogor Mawtribes",
+	  "Ossiarch Bonereapers",
+	  "Soulblight Gravelords",
+	  "Nighthaunt",
+	  "Flesh-eater Courts"
+    ],
+    aliases: [
+      "big drogg fort-kicka"
+    ],
+  },
+  {
+    name: "Bundo Whalebiter",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Ironjawz",
+	  "Kruleboyz",
+	  "Gloomspite Gitz",
+	  "Ogor Mawtribes",
+	  "Stormcast Eternals",
+	  "Cities of Sigmar",
+	  "Seraphon",
+	  "Lumineth Realm-lords",
+	  "Daughters of Khaine",
+	  "Idoneth Deepkin",
+	  "Sylvaneth",
+	  "Kharadron Overlords",
+	  "Fyreslayers"
+    ],
+    aliases: [
+      "bundo whalebiter"
+    ],
+  },
+  {
+    name: "Enforcers of the Tithe",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Flesh-eater Courts",
+	  "Fyreslayers",
+	  "Gloomspite Gitz",
+	  "Helsmiths of Hashut",
+	  "Kharadron Overlords",
+	  "Kruleboyz",
+	  "Ogor Mawtribes",
+	  "Skaven",
+	  "Sons of Behemat",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "enforcers of the tithe"
+    ],
+  },
+  {
+    name: "Exile Scavengers",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Fyreslayers",
+	  "Gloomspite Gitz",
+	  "Ironjawz",
+	  "Kruleboyz",
+	  "Lumineth Realm-lords",
+	  "Ogor Mawtribes",
+	  "Ossiarch Bonereapers",
+	  "Seraphon",
+	  "Slaves to Darkness",
+	  "Sons of Behemat",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "exile scavengers"
+    ],
+  },
+  {
+    name: "Goroan Scions",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Gloomspite Gitz",
+	  "Ironjawz",
+	  "Kruleboyz",
+	  "Ogor Mawtribes",
+	  "Sons of Behemat"
+    ],
+    aliases: [
+      "goroan scions"
+    ],
+  },
+  {
+    name: "Nurgle's Gift",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Flesh-eater Courts",
+	  "Fyreslayers",
+	  "Gloomspite Gitz",
+	  "Helsmiths of Hashut",
+	  "Ironjawz",
+	  "Kharadron Overlords",
+	  "Kruleboyz",
+	  "Lumineth Realm-lords",
+	  "Ogor Mawtribes",
+	  "Skaven",
+	  "Slaves to Darkness",
+	  "Sons of Behemat",
+	  "Sylvaneth"
+    ],
+    aliases: [
+      "nurgle's gift"
+    ],
+  },
+  {
+    name: "One-eyed Grunnock",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Ironjawz",
+	  "Kruleboyz",
+	  "Gloomspite Gitz",
+	  "Ogor Mawtribes",
+	  "Skaven",
+	  "Slaves to Darkness",
+	  "Blades of Khorne",
+	  "Disciples of Tzeentch",
+	  "Maggotkin of Nurgle",
+	  "Hedonites of Slaanesh",
+	  "Helsmiths of Hashut"
+    ],
+    aliases: [
+      "one-eyed grunnock",
+	  "One-Eyed Grunnock"
+    ],
+  },
+  {
+    name: "Snerk's Trogg-Fer-Hire",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Blades of Khorne",
+	  "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Hedonites of Slaanesh",
+	  "Idoneth Deepkin",
+	  "Ironjawz",
+	  "Kruleboyz",
+	  "Lumineth Realm-lords",
+	  "Maggotkin of Nurgle",
+	  "Ogor Mawtribes",
+	  "Ossiarch Bonereapers",
+	  "Seraphon",
+	  "Skaven",
+	  "Slaves to Darkness",
+	  "Soulblight Gravelords",
+	  "Stormcast Eternals"
+    ],
+    aliases: [
+      "snerk's trogg-fer-hire"
+    ],
+  },
+  {
+    name: "Squires of the Everchosen",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Blades of Khorne",
+	  "Disciples of Tzeentch",
+	  "Maggotkin of Nurgle",
+	  "Hedonites of Slaanesh",
+	  "Slaves to Darkness"
+    ],
+    aliases: [
+      "squires of the everchosen"
+    ],
+  },
+  {
+    name: "Stumblefoot Gargant",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Blades of Khorne",
+	  "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Disciples of Tzeentch",
+	  "Flesh-eater Courts",
+	  "Fyreslayers",
+	  "Gloomspite Gitz",
+	  "Hedonites of Slaanesh",
+	  "Helsmiths of Hashut",
+	  "Idoneth Deepkin",
+	  "Ironjawz",
+	  "Kharadron Overlords",
+	  "Kruleboyz",
+	  "Lumineth Realm-lords",
+	  "Maggotkin of Nurgle",
+	  "Nighthaunt",
+	  "Ogor Mawtribes",
+	  "Ossiarch Bonereapers",
+	  "Seraphon",
+	  "Skaven",
+	  "Slaves to Darkness",
+	  "Soulblight Gravelords",
+	  "Stormcast Eternals",
+	  "Sylvaneth"
+    ],
+    aliases: [
+      "stumblefoot gargant"
+    ],
+  },
+  {
+    name: "The Lost-Song Spirits",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Idoneth Deepkin",
+	  "Nighthaunt",
+	  "Ossiarch Bonereapers",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "the lost-song spirits"
+    ],
+  },
+  {
+    name: "Big Grikk's Kruleshots",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Gloomspite Gitz",
+	  "Ironjawz",
+	  "Ogor Mawtribes",
+	  "Sons of Behemat"
+    ],
+    aliases: [
+      "big grikk's kruleshots"
+    ],
+  },
+  {
+    name: "Braggit's Bottle-Snatchaz",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Ironjawz",
+	  "Kruleboyz",
+	  "Ogor Mawtribes",
+	  "Sons of Behemat"
+    ],
+    aliases: [
+      "braggit's bottle-snatchaz"
+    ],
+  },
+  {
+    name: "Brand's Oathbound",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Blades of Khorne",
+	  "Disciples of Tzeentch",
+	  "Maggotkin of Nurgle",
+	  "Hedonites of Slaanesh",
+	  "Helsmiths of Hashut",
+	  "Skaven"
+    ],
+    aliases: [
+      "brand's oathbound"
+    ],
+  },
+  {
+    name: "Elthwin's Thorns",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Fyreslayers",
+	  "Idoneth Deepkin",
+	  "Kharadron Overlords",
+	  "Lumineth Realm-lords",
+	  "Seraphon",
+	  "Stormcast Eternals"
+    ],
+    aliases: [
+      "elthwin's thorns"
+    ],
+  },
+  {
+    name: "Fjori's Flamebearers",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Sylvaneth",
+	  "Idoneth Deepkin",
+	  "Kharadron Overlords",
+	  "Lumineth Realm-lords",
+	  "Seraphon",
+	  "Stormcast Eternals"
+    ],
+    aliases: [
+      "fjori's flamebearers"
+    ],
+  },
+  {
+    name: "Gotrek Gurnisson",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Sylvaneth",
+	  "Idoneth Deepkin",
+	  "Kharadron Overlords",
+	  "Lumineth Realm-lords",
+	  "Seraphon",
+	  "Stormcast Eternals",
+	  "Fyreslayers"
+    ],
+    aliases: [
+      "gotrek gurnisson"
+    ],
+  },
+  {
+    name: "Hargax's Pit-Beasts",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Blades of Khorne",
+	  "Disciples of Tzeentch",
+	  "Maggotkin of Nurgle",
+	  "Hedonites of Slaanesh",
+	  "Helsmiths of Hashut",
+	  "Skaven"
+    ],
+    aliases: [
+      "hargax's pit-beasts"
+    ],
+  },
+  {
+    name: "Jerrion's Delegation",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Nighthaunt",
+	  "Ossiarch Bonereapers",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "jerrion's delegation"
+    ],
+  },
+  {
+    name: "Neferata's Royal Echelon",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Nighthaunt",
+	  "Ossiarch Bonereapers"
+    ],
+    aliases: [
+      "neferata's royal echelon"
+    ],
+  },
+  {
+    name: "Norgimm's Rune Throng",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Fyreslayers",
+	  "Daughters of Khaine",
+	  "Sylvaneth",
+	  "Idoneth Deepkin",
+	  "Kharadron Overlords",
+	  "Lumineth Realm-lords",
+	  "Seraphon",
+	  "Stormcast Eternals"
+    ],
+    aliases: [
+      "norgrimm's rune throng"
+    ],
+  },
+  {
+    name: "Phulgoth's Shudderhood",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Blades of Khorne",
+	  "Disciples of Tzeentch",
+	  "Hedonites of Slaanesh",
+	  "Helsmiths of Hashut",
+	  "Slaves to Darkness",
+	  "Skaven"
+    ],
+    aliases: [
+      "phulgoth's shudderhood"
+    ],
+  },
+  {
+    name: "Saviours of Cinderfall",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Fyreslayers",
+	  "Daughters of Khaine",
+	  "Sylvaneth",
+	  "Idoneth Deepkin",
+	  "Kharadron Overlords",
+	  "Lumineth Realm-lords",
+	  "Seraphon",
+	  "Stormcast Eternals"
+    ],
+    aliases: [
+      "saviours of cinderfall"
+    ],
+  },
+  {
+    name: "Scions of the Necropolis",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Nighthaunt",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "scions of the necropolis"
+    ],
+  },
+  {
+    name: "The Blacktalons",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Cities of Sigmar",
+	  "Daughters of Khaine",
+	  "Sylvaneth",
+	  "Idoneth Deepkin",
+	  "Kharadron Overlords",
+	  "Lumineth Realm-lords",
+	  "Seraphon",
+	  "Fyreslayers"
+    ],
+    aliases: [
+      "the blacktalons"
+    ],
+  },
+  {
+    name: "The Coven of Thryx",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Maggotkin of Nurgle",
+	  "Hedonites of Slaanesh",
+	  "Slaves to Darkness",
+	  "Skaven"
+    ],
+    aliases: [
+      "the coven of thryx"
+    ],
+  },
+  {
+    name: "The Liche's Hand",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Nighthaunt",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "the liche's hand"
+    ],
+  },
+  {
+    name: "The Sorrowmourn Choir",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Ossiarch Bonereapers",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "the sorrowmourn choir"
+    ],
+  },
+  {
+    name: "The Sternieste Garrison",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Nighthaunt",
+	  "Ossiarch Bonereapers"
+    ],
+    aliases: [
+      "the sternieste garrison"
+    ],
+  },
+  {
+    name: "The Summerking's Entourage",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Nighthaunt",
+	  "Ossiarch Bonereapers",
+	  "Soulblight Gravelords"
+    ],
+    aliases: [
+      "the summerking's entourage"
+    ],
+  },
+  {
+    name: "Veremord's Shamblers",
+    type: "regiment_of_renown",
+    allowedFactions: [
+      "Flesh-eater Courts",
+	  "Nighthaunt",
+	  "Ossiarch Bonereapers"
+    ],
+    aliases: [
+      "veremord's shamblers"
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default REGIMENTS_OF_RENOWN;
