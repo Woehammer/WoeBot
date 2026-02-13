5454ed10d9a23c2ee3b5ecdf849a6f15411ee226
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-13 16:56:00 +0000

    Create battleplans.js

---
+++ src/data/aos/battleplans.js
@@ -0,0 +1,114 @@
+// ==================================================
+// LOOKUP: AOS BATTLEPLANS
+// PURPOSE: Canonical battleplan names + aliases
+// ==================================================
+
+// ==================================================
+// REGIMENT DEFINITIONS
+// ==================================================
+
+export const BATTLEPLANS = [
+  {
+    name: "Passing Seasons",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "passing seasons"
+    ],
+  },
+  {
+    name: "Paths of the Fey",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "paths of the fey"
+    ],
+  },
+  {
+    name: "Roiling Roots",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "roiling roots"
+    ],
+  },
+  {
+    name: "Cyclic Shifts",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "cyclic shifts"
+    ],
+  },
+  {
+    name: "Surge of Slaughter",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "surge of slaughter"
+    ],
+  },
+  {
+    name: "Linked Ley Lines",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "linked ley lines"
+    ],
+  },
+  {
+    name: "Noxious Nexus",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "noxious nexus"
+    ],
+  },
+  {
+    name: "The Liferoots",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "the liferoots"
+    ],
+  },
+  {
+    name: "Bountiful Equinox",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "bountiful equinox"
+    ],
+  },
+  {
+    name: "Lifecycle",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "lifecycle",
+	  "life cycle"
+    ],
+  },
+  {
+    name: "Creeping Corruption",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "creeping corruption"
+    ],
+  },
+  {
+    name: "Grasp of Thorns",
+    type: "battleplan",
+	season: "GHB 2025-26",
+    aliases: [	
+      "grasp of thorns"
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default BATTLEPLANS;
