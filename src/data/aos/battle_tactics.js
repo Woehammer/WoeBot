9a47c5d1fb771cf7d59a44908e938f56f634ab88
Woehammer <72220895+Woehammer@users.noreply.github.com>
2026-02-13 16:49:57 +0000

    Create battle_tactics.js

---
+++ src/data/aos/battle_tactics.js
@@ -0,0 +1,65 @@
+// ==================================================
+// LOOKUP: AOS BATTLE TACTICS
+// PURPOSE: Canonical battle tactic names + aliases
+// ==================================================
+
+// ==================================================
+// REGIMENT DEFINITIONS
+// ==================================================
+
+export const BATTLE_TACTICS = [
+  {
+    name: "Master the Paths",
+    type: "battle_tactic",
+	season: "GHB 2025-26",
+    aliases: [	
+      "master the paths"
+    ],
+  },
+  {
+    name: "Restless Energy",
+    type: "battle_tactic",
+	season: "GHB 2025-26",
+    aliases: [	
+      "restless energy"
+    ],
+  },
+  {
+    name: "Intercept and Recover",
+    type: "battle_tactic",
+	season: "GHB 2025-26",
+    aliases: [	
+      "intercept and recover"
+    ],
+  },
+  {
+    name: "Wrathful Cycles",
+    type: "battle_tactic",
+	season: "GHB 2025-26",
+    aliases: [	
+      "wrathful cycles"
+    ],
+  },
+  {
+    name: "Scouting Force",
+    type: "battle_tactic",
+	season: "GHB 2025-26",
+    aliases: [	
+      "scouting force"
+    ],
+  },
+  {
+    name: "Attuned to Ghyran",
+    type: "battle_tactic",
+	season: "GHB 2025-26",
+    aliases: [	
+      "attuned to ghyran"
+    ],
+  },
+];
+
+// ==================================================
+// EXPORTS
+// ==================================================
+
+export default BATTLE_TACTICS;
